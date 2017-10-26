f2b975968104f6eecd00e8b2e9bf8bcc5a7b4c1e
tanaka-kazu <asterisk.tanaka@gmail.com>
2017-10-26 23:37:55 +0900

    プロトタイプ完成

--- src/ywtListView.js
+++ src/ywtListView.js
@@ -25,7 +25,7 @@ kintone.events.on('app.record.index.show', (event) => {
 
   const fileDownload = async function (fileKey, rowIndex) {
     const response = await kintoneUtility.rest.downloadFile({
-      fileKey: fileKey
+      fileKey
     });
     // プロフィール画像レンダリング
     const elems = document.querySelectorAll(".icon");
@@ -34,7 +34,7 @@ kintone.events.on('app.record.index.show', (event) => {
   };
 
   (async () => {
-    // YKTアプリのレコードを取得
+    // YWTアプリのレコードを取得
     const { records: mainRecords } = await kintoneUtility.rest.getRecords({
       app: kintone.app.getId(),
       query: kintone.app.getQuery(),
@@ -53,13 +53,84 @@ kintone.events.on('app.record.index.show', (event) => {
             fileDownload(fileKey, i);
           }
         }
-      };
+      }
       return r;
     });
 
     new Vue({
       // Vueを適用するelement
       el: '#customize_view',
+      methods: {
+        showDetail: function (record, rowIndex) {
+          // 行選択表示
+          const rows = document.querySelectorAll('.list_row');
+          rows.forEach(function (row, i) {
+            if (i == rowIndex) {
+              row.classList.add('row_selected');
+            }
+            else {
+              row.classList.remove('row_selected');
+            }
+          });
+          document.getElementById('id').value = record.レコード番号.value;
+          document.getElementById('memo').value = record.memo.value;
+          document.getElementById('feedback').value = record.feedback.value;
+          document.getElementById('y').value = record.comment_y.value;
+          document.getElementById('w').value = record.comment_w.value;
+          document.getElementById('t').value = record.comment_t.value;
+        },
+        edit: function (record) {
+          location.href = "/k/3/edit?record=" + record.レコード番号.value;
+        },
+        update: function () {
+          const id = document.getElementById('id').value;
+          if (!id) {
+            return;
+          }
+          const memo = document.getElementById('memo').value;
+          const feedback = document.getElementById('feedback').value;
+          const comment_y = document.getElementById('y').value;
+          const comment_w = document.getElementById('w').value;
+          const comment_t = document.getElementById('t').value;
+          const record = {
+            memo: {
+              value: memo
+            },
+            feedback: {
+              value: feedback
+            },
+            comment_y: {
+              value: comment_y
+            },
+            comment_w: {
+              value: comment_w
+            },
+            comment_t: {
+              value: comment_t
+            },
+          };
+          kintoneUtility.rest.putRecord({
+            app: kintone.app.getId(),
+            id: id,
+            record: record
+          }).then(function (response) {
+            // ローカルデータに反映
+            for (const rec of records) {
+              if (rec.レコード番号.value === id) {
+                rec.memo.value = memo;
+                rec.feedback.value = feedback;
+                rec.comment_y.value = comment_y;
+                rec.comment_w.value = comment_w;
+                rec.comment_t.value = comment_t;
+                break;
+              }
+            }
+            alert("変更を保存しました．");
+          }).catch(function (error) {
+            alert("変更の保存に失敗しました．¥n" + JSON.stringify(error));
+          });;
+        }
+      },
       data: {
         records,
       },
