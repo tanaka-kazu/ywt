2cdafb5511a52149cae2b3607ec90195369cdd6d
tanaka-kazu <asterisk.tanaka@gmail.com>
2017-11-02 00:35:39 +0900

    再レビュー指摘反映／編集モードの不具合修正

--- src/ywtListView.js
+++ src/ywtListView.js
@@ -19,154 +19,146 @@ kintone.events.on('app.record.index.show', event => {
 
   const user = kintone.getLoginUser()
   let isModify = false
+  render()
 
-    ; (async () => {
-      // YWTアプリのレコードを取得
-      const { records: mainRecords } = await kintoneUtility.rest.getRecords({
-        app: kintone.app.getId(),
-        query: kintone.app.getQuery()
-      })
+  async function render() {
+    // YWTアプリのレコードを取得
+    const { records: mainRecords } = await kintoneUtility.rest.getRecords({
+      app: kintone.app.getId(),
+      query: kintone.app.getQuery()
+    })
 
-      const { records: optRecords } = await kintoneUtility.rest.getRecords({
-        app: kintone.app.getLookupTargetAppId('mail')
-      })
+    const { records: optRecords } = await kintoneUtility.rest.getRecords({
+      app: kintone.app.getLookupTargetAppId('mail')
+    })
 
-      const records = mainRecords.map((r, i) => {
-        // プロフィール画像取得
-        for (const o of optRecords) {
-          if (r.mail.value === o.mail.value) {
-            r.fileKey =
-              o.profile_img.value.length > 0
-                ? o.profile_img.value[0].fileKey
-                : null
-          }
+    const records = mainRecords.map((r, i) => {
+      // プロフィール画像取得
+      for (const o of optRecords) {
+        if (r.mail.value === o.mail.value) {
+          r.fileKey =
+            o.profile_img.value.length > 0
+              ? o.profile_img.value[0].fileKey
+              : null
         }
-        // レコード作成者以外は編集不可
-        r.editable = r.作成者.value.code === user.code
-        // プロフィール画像URL
-        r.profile_img = ''
-        // 行選択フラグ
-        r.selected = false
-
-        return r
-      })
-
-      const form = {
-        id: '',
-        memo: '',
-        feedback: '',
-        commentY: '',
-        commentW: '',
-        commentT: '',
-        editable: false
       }
+      // レコード作成者以外は編集不可
+      r.editable = r.作成者.value.code === user.code
+      // プロフィール画像URL
+      r.profile_img = ''
+      // 行選択フラグ
+      r.selected = false
 
-      Vue.component('icon-img', {
-        template:
-        '\
-        <img class="icon" v-bind:src="record.profile_img" v-on:load="imgLoad" width="40" height="40" />\
-      ',
-        props: ['record'],
-        computed: {
-          // アイコン用画像設定
-          imgLoad: async function (component) {
-            const response = await kintoneUtility.rest.downloadFile({
-              fileKey: this.record.fileKey
-            })
-            // プロフィール画像レンダリング
-            const url = window.URL || window.webkitURL
-            this.record.profile_img = url.createObjectURL(response)
-          }
+      return r
+    })
+
+    const form = {
+      id: '',
+      memo: '',
+      feedback: '',
+      commentY: '',
+      commentW: '',
+      commentT: '',
+      editable: false
+    }
+
+    Vue.component('icon-img', {
+      template: '<img class="icon" v-bind:src="record.profile_img" v-on:load="imgLoad" width="40" height="40" />',
+      props: ['record'],
+      computed: {
+        // アイコン用画像設定
+        imgLoad: async function (component) {
+          const response = await kintoneUtility.rest.downloadFile({
+            fileKey: this.record.fileKey
+          })
+          // プロフィール画像レンダリング
+          const url = window.URL || window.webkitURL
+          this.record.profile_img = url.createObjectURL(response)
         }
-      })
+      }
+    })
 
-      const vm = new Vue({
-        // Vueを適用するelement
-        el: '#customize-view',
-        methods: {
-          showDetail: function (record) {
-            if (isModify && !confirm('変更を破棄しますか？')) {
-              return
-            }
-            // 行選択表示
-            for (const rec of this.records) {
-              if (rec.レコード番号.value == record.レコード番号.value) {
-                rec.selected = true
-              } else {
-                rec.selected = false
-              }
-            }
-            form.memoStyle = record.editable ? 'inherit' : 'none'
-            form.editable = record.editable
-            form.id = record.レコード番号.value
-            form.memo = record.memo.value
-            form.feedback = record.feedback.value
-            form.commentY = record.comment_y.value
-            form.commentW = record.comment_w.value
-            form.commentT = record.comment_t.value
-            isModify = false
-          },
-          edit: function (record) {
-            location.href = '/k/3/edit?record=' + record.レコード番号.value
-          },
-          update: function () {
-            if (!isModify) {
-              return
-            }
-            if (!form.id) {
-              return
-            }
-            const record = {
-              memo: {
-                value: form.memo
-              },
-              feedback: {
-                value: form.feedback
-              },
-              comment_y: {
-                value: form.commentY
-              },
-              comment_w: {
-                value: form.commentW
-              },
-              comment_t: {
-                value: form.commentT
-              }
-            }
-            kintoneUtility.rest
-              .putRecord({
-                app: kintone.app.getId(),
-                id: form.id,
-                record
-              })
-              .then(function (response) {
-                // ローカルデータに反映
-                for (const rec of records) {
-                  if (rec.レコード番号.value === form.id) {
-                    rec.memo.value = form.memo
-                    rec.feedback.value = form.feedback
-                    rec.comment_y.value = form.commentY
-                    rec.comment_w.value = form.commentW
-                    rec.comment_t.value = form.commentT
-                    break
-                  }
-                }
-                isModify = false
-                alert('変更を保存しました．')
-              })
-              .catch(function (error) {
-                alert('変更の保存に失敗しました．\n' + JSON.stringify(error))
-              })
-          },
-          updateForm: function () {
-            isModify = true
+    new Vue({
+      // Vueを適用するelement
+      el: '#customize-view',
+      methods: {
+        showDetail: function (record) {
+          if (isModify && !confirm('変更を破棄しますか？')) {
+            return
           }
+          // 行選択表示
+          for (const rec of this.records) {
+            rec.selected = rec.レコード番号.value === record.レコード番号.value
+          }
+          form.memoStyle = record.editable ? 'inherit' : 'none'
+          form.editable = record.editable
+          form.id = record.レコード番号.value
+          form.memo = record.memo.value
+          form.feedback = record.feedback.value
+          form.commentY = record.comment_y.value
+          form.commentW = record.comment_w.value
+          form.commentT = record.comment_t.value
+          isModify = false
         },
-        data: {
-          records,
-          isModify,
-          form
+        edit: function (record) {
+          const id = record.レコード番号.value
+          location.href = '/k/3/show#record=' + id + '&l.view=' + id + '&l.q&mode=edit'
+        },
+        update: function () {
+          if (!isModify || !form.id) {
+            return
+          }
+          const record = {
+            memo: {
+              value: form.memo
+            },
+            feedback: {
+              value: form.feedback
+            },
+            comment_y: {
+              value: form.commentY
+            },
+            comment_w: {
+              value: form.commentW
+            },
+            comment_t: {
+              value: form.commentT
+            }
+          }
+          kintoneUtility.rest
+            .putRecord({
+              app: kintone.app.getId(),
+              id: form.id,
+              record
+            })
+            .then(function (response) {
+              // ローカルデータに反映
+              for (const rec of records) {
+                if (rec.レコード番号.value === form.id) {
+                  rec.memo.value = form.memo
+                  rec.feedback.value = form.feedback
+                  rec.comment_y.value = form.commentY
+                  rec.comment_w.value = form.commentW
+                  rec.comment_t.value = form.commentT
+                  break
+                }
+              }
+              isModify = false
+              alert('変更を保存しました．')
+            })
+            .catch(function (error) {
+              alert('変更の保存に失敗しました．\n' + JSON.stringify(error))
+            })
+        },
+        updateForm: function () {
+          isModify = true
         }
-      })
-    })()
+      },
+      data: {
+        records,
+        isModify,
+        form
+      }
+    })
+  }
 })
